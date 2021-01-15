5c4b5a65027386c04a29f3987dbae8f7f471b699
Cem Dırman <cemdrman@gmail.com>
2021-01-16 02:25:56 +0300

    fixed route

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ var app = express();
 var dbConnection = require("./db/DbConnection");
 var ProductController = require('./controller/ProductController');
 var express = require('express');
-var router = express.Router();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('api/products', ProductController);
+app.use('/api/products', ProductController);
 
 module.exports = app;
