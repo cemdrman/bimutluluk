b275c1d214a16926b0cabbd6fe6e97265563e26e
Cem Dırman <cemdrman@gmail.com>
2021-01-16 02:04:17 +0300

    mongo connection

---
+++ db/DbConnection.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true",{ useNewUrlParser:true, useUnifiedTopology: true });
