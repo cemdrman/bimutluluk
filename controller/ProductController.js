561bdc1870f136b5729eeac9769e09cff28c17a3
Cem Dırman <cemdrman@gmail.com>
2021-01-16 14:43:35 +0300

    date control

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -4,16 +4,28 @@ var Record = require("../model/Record");
 var responseBuilder = require("../response/responseBuilder");
 
 router.post('/', function (req, res) {
-    var reqString = req.body.startDate +' ' + req.body.endDate + ' ' +req.body.minCount + ' ' +req.body.maxCount;
+    var reqString = req.body.startDate + ' ' + req.body.endDate + ' ' + req.body.minCount + ' ' + req.body.maxCount;
     console.debug('Request: ' + reqString);
     //validate request
-    Record.find({
-        },  function (err, records) {
-                if (err) {
-                    return res.status(500).send("There was a problem adding the information to the database.");
-                }
-                res.status(200).send(responseBuilder.success(0,"Success",records));
-        });
+
+    const query = Record.find({
+        createdAt: {
+            $gte: req.body.startDate,
+            $lt: req.body.endDate
+        }
+    }).sort({
+        createdAt: 'asc'
+    });
+    // selecting the 'key', 2createdAt' and 'totalCount' fields
+    query.select('key createdAt totalCount');
+    // execute the query at a later time
+    query.exec(function (err, records) {
+
+        if (err) {
+            return res.status(500).send("There was a problem adding the information to the database.");
+        }
+        res.status(200).send(responseBuilder.success(0, "Success", records));
+    });
 });
 
 module.exports = router;
