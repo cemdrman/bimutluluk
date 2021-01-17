c77e488d8fe8bae82bcfc5ccdb7fa645203d3ce1
Cem Dırman <cemdrman@gmail.com>
2021-01-17 15:53:23 +0300

    global exception handler

--- src/app/index.js
+++ src/app/index.js
@@ -4,13 +4,15 @@ var ProductController = require('./controller/recordController');
 var express = require('express');
 var bodyParser = require('body-parser');
 require('dotenv').config();
-
+var exceptionHandler = require("./exception/exceptionHandler");
 var port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+// Handle errors
+app.use(exceptionHandler);
 
 app.use('/api/records', ProductController);
 
