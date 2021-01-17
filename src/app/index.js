048240f5d9de86094db1a5a53cd7fe9ded659307
Cem Dırman <cemdrman@gmail.com>
2021-01-17 13:21:44 +0300

    refactoring

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,16 @@
-var app = require("./app");
+var express = require("express");
+var app = express();
+var ProductController = require('./controller/ProductController');
+var express = require('express');
+var bodyParser = require('body-parser');
 var port = process.env.port || 5000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use('/api/products', ProductController);
 
 app.listen(port, function () {
     console.log('Express Server Listening ' + port);
