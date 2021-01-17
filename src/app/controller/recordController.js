dd09d964db0f08b30aadfa6248031763c23bff99
Cem Dırman <cemdrman@gmail.com>
2021-01-17 14:35:46 +0300

    modified

--- src/app/controller/recordController.js
+++ src/app/controller/recordController.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var recordService = require("../service/RecordService");
-var responseBuilder = require("../response/ResponseBuilder");
+var recordService = require("../service/recordService");
+var responseBuilder = require("../response/responseBuilder");
 
 const getRecords = async (req, res, next) => {
     try {
